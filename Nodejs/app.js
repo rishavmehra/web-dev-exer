0daad31a9fa9d7081c4100d326b480e3a5fab157
rishavmehra <rishavmehra61@gmail.com>
2023-01-24 23:02:02 +0530

    nodejs

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,13 +1,9 @@
-const rishavHTTP = require('http')
+const rishavHTTP = require('http');
 
-// function rqlisner(res, req) {   //we can pass this function in createServer function - or we make anonymous function in the createServer 
-//     console.log(req);    
-// }
+const routes = require('./routes'); 
 
-const rishavlis = rishavHTTP.createServer((req, res)/* this the anonlymous function */ =>{
-    console.log(req);
-});
+console.log(routes.someText);
 
-rishavlis.listen(3000);
+const server = rishavHTTP.createServer(routes.handler);
 
-
+server.listen(3000);
---
+++ Nodejs/routes.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+
+const requestHandler = (req, res) =>{  
+    const url = req.url; 
+    const method =  req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Form Massage</title></head>');
+        res.write('<body><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/massage' && method==='POST') {
+        const body = [];
+        req.on("data", (chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        return req.on("end", ()=>{  // we don't need return the when return in end  
+            const parseBody = Buffer.concat(body).toString();
+            const massage = parseBody.split("=")[1]
+            // fs.writeFileSync("massage.txt", massage)
+            fs.writeFile("massage.txt", massage, (err) => { 
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            } )
+            console.log(parseBody);
+        })  
+        // res.statusCode = 302;
+        // res.setHeader('Location', '/')
+        // return res.end(); 
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>This the nodejs page title</title></head>');
+    res.write('<body><h1>Hey from nodejs Server</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+// module.exports = requestHandler; //1st way to export
+
+module.exports = { // 2nd way to export
+    handler: requestHandler,
+    someText: 'Some hard coded text'
+}
+
+// module.exports.handler = requestHandler; // 3rd way to export
+// module.exports.someText = 'Some hard coded text';
+
+//exports.handler = requestHandler; // 4th way to export
+//exports.someText = 'Some hard coded text';
